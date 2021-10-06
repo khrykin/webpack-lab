4891abeeb283b975ba608d6e76434356ebfbd27c
Dmitry Khrykin <khrykin@me.com>
2021-10-06 09:14:28 +0300

    dead code in huge.js

--- make-lib.js
+++ make-lib.js
@@ -8,13 +8,16 @@ const size = Number(process.argv[3]);
 
 const stream = fs.createWriteStream(filePath);
 
-stream.write(`const ${filename} = "`);
+stream.write(`let ${filename};\n`);
+stream.write(`if (false) {\n`);
+stream.write(`${filename} = "\n`);
 
 for (let i = 0; i < size / 2; i++) {
   stream.write(crypto.randomBytes(1).toString('hex'));
 }
 
 stream.write(`";\n`);
+stream.write(`}"`);
 stream.write(`console.log("${filename} executed");\n`);
 stream.write(`export default ${filename};`);
 stream.end();
--- src/with-huge.tsx
+++ src/with-huge.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { useEffect, useState } from "react";
 import loadHuge from "./loadHuge";
 
 export default function WithHuge({ from }: { from: string; }) {
